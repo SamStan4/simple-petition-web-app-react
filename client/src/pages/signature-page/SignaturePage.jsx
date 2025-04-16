d1f0fc57e32e70e86088c247c460bd85b6d2ddba
samStan4 <sam.charles.stanley@gmail.com>
2025-04-16 11:27:27 -0700

    started form page

--- client/src/pages/signature-page/SignaturePage.jsx
+++ client/src/pages/signature-page/SignaturePage.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./SignaturePageStyles.css";
 
 export default function SignaturePage() {
+  const [signatures, setSignatures] = useState([]);
+  useEffect(() => {
+    // TODO: set the signatures
+  }, []);
   return (
     <div className="page-wrapper">
+      <div className="form-wrapper">
+
+      </div>
     </div>
   );
 };
